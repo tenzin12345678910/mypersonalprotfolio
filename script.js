4ec4b6f42f60dcce62476c3c3538e36848108170
Tenzin Woesel <tenzinw@csedge.org>
2019-05-20 14:05:34 -0700

    ;

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let myAlbum2 = {
 let infoDisplay2 = `
   <h1> Album Name: ${myAlbum2["name"]} </h1>
   <p> Cost: ${myAlbum2["price"]} </p>
-  <p> Fan Rating: ${myAlbum2["rating"]} out of 20 </p>
+  <p> Fan Rating: ${myAlbum2["rating"]} out of 5 </p>
 `
 
 let summaryDisplay2 = `
@@ -49,12 +49,193 @@ let myAlbum3 = {
 let infoDisplay3 = `
   <h1> Album Name: ${myAlbum3["name"]} </h1>
   <p> Cost: ${myAlbum3["price"]} </p>
-  <p> Fan Rating: ${myAlbum3["rating"]} out of 20 </p>
+  <p> Fan Rating: ${myAlbum3["rating"]} out of 5 </p>
 `
 
 let summaryDisplay3 = `
 <p> Official Summary: ${myAlbum3["summary"]} </p>
 `
-$("#album-info2").append(infoDisplay3)
-$("#album-summary2").append(summaryDisplay3)
+$("#album-info3").append(infoDisplay3)
+$("#album-summary3").append(summaryDisplay3)
+let myAlbum4 = {
+  name: "LOVE YOURSELF承‘HER’",
+  price: "$19",
+  rating: 4.8,
+  summary: "LOVE YOURSELF 承 'HER' is an expression of the anxiety and elation of love, told in the unique style of BTS. The “love” that BTS aims to convey in the LOVE YOURSELF series is both the individual experience of a boy growing into adulthood and a message of peace and unity to our society today. In the mini album 承 'HER' that begins this new narrative, the image of boys in love for the first time are expressed in a refreshing and cheerful take that aims to talk about the topic of “love” while immersed in a coming-of-age tale."
+}
+
+let infoDisplay4 = `
+  <h1> Album Name: ${myAlbum4["name"]} </h1>
+  <p> Cost: ${myAlbum4["price"]} </p>
+  <p> Fan Rating: ${myAlbum4["rating"]} out of 5 </p>
+`
+
+let summaryDisplay4 = `
+<p> Official Summary: ${myAlbum4["summary"]} </p>
+`
+$("#album-info4").append(infoDisplay4)
+$("#album-summary4").append(summaryDisplay4)
+let myAlbum5 = {
+  name: "YOU NEVER WALK ALONE",
+  price: "$19",
+  rating: 4.5,
+  summary: "YOU NEVER WALK ALONE completes the stories of youth and growth that could not all be contained in the WINGS album. Whereas The most beautiful moment in life series and WINGS were narratives of youth and growth, this additional chapter to WINGS is a message of warm consolation and hope for the suffering youth of this generation."
+}
+
+let infoDisplay5 = `
+  <h1> Album Name: ${myAlbum5["name"]} </h1>
+  <p> Cost: ${myAlbum5["price"]} </p>
+  <p> Fan Rating: ${myAlbum5["rating"]} out of 5 </p>
+`
+
+let summaryDisplay5 = `
+<p> Official Summary: ${myAlbum5["summary"]} </p>
+`
+$("#album-info5").append(infoDisplay4)
+$("#album-summary5").append(summaryDisplay4)
+let myAlbum6 = {
+  name: "WINGS",
+  price: "$19",
+  rating: 4.6,
+  summary: "BTS will release their second studio album, WINGS. Having met their most beautiful moment in life through the ablum series of the same title, BTS now stands before a new door. Set for release on October 10, WINGS contains songs about boys who encounter temptation for the first time and must ponder and agonize in the face of it. As the seven boys experience pain and bliss, they bring forth the image of birds that break out of their shells and try to take flight for the first time."
+}
+
+let infoDisplay6 = `
+  <h1> Album Name: ${myAlbum6["name"]} </h1>
+  <p> Cost: ${myAlbum6["price"]} </p>
+  <p> Fan Rating: ${myAlbum6["rating"]} out of 5 </p>
+`
+
+let summaryDisplay6 = `
+<p> Official Summary: ${myAlbum6["summary"]} </p>
+`
+$("#album-info6").append(infoDisplay4)
+$("#album-summary6").append(summaryDisplay4)
+let myAlbum7 = {
+  name: "THE MOST BEAUTIFUL MOMENT IN LIFE : YOUNG FOREVER",
+  price: "$29.65",
+  rating: 4.9,
+  summary: "Having won over the world with their two-part The most beautiful moment in life series in 2015, BTS will release their special album The most beautiful moment in life : YOUNG FOREVER on May 2. True to the title of the albums, the series has enabled BTS to now be in the most beautiful moment of their lives. The most beautiful moment in life : YOUNG FOREVER is a special album that marks the conclusion of the epic journey of the series, containing the last stories told by young people who, despite an uncertain and insecure reality (The most beautiful moment in life pt.1) continue to surge forward (The most beautiful moment in life pt.2)."
+}
+
+let infoDisplay7 = `
+  <h1> Album Name: ${myAlbum7["name"]} </h1>
+  <p> Cost: ${myAlbum7["price"]} </p>
+  <p> Fan Rating: ${myAlbum7["rating"]} out of 5 </p>
+`
+
+let summaryDisplay7 = `
+<p> Official Summary: ${myAlbum7["summary"]} </p>
+`
+$("#album-info7").append(infoDisplay4)
+$("#album-summary7").append(summaryDisplay4)
+let myAlbum8 = {
+  name: "THE MOST BEAUTIFUL MOMENT IN LIFE PT.2",
+  price: "$22.98",
+  rating: 4.5,
+  summary: "Seven young men on the threshold of their 20s. Now entering their young adulthood, the members of BTS sang in their previous album, The most beautiful moment in life pt.1, about people their age being threatened by uncertainty. Released seven months after that first installment, The most beautiful moment in life pt.2 sings about the energy of youth barreling forward, embracing even the uncertainty and insecurity. Now beginning to taste the bitterness of grown-up life, they can easily sense that life isn't always beautiful. However, they continue to sprint forward, relying only on their two destitute legs and the fire in their hearts. There’s nothing else to do but keep running. Facing the powerlessness of one’s youth, they collide and shatter and still cry out that “it’s okay to fall, it’s okay to get hurt.” That is the beauty of this moment. Continuing the tradition of the previous album, the members including RM, SUGA, j-hope, V and Jung Kook add their names to the producing and songwriting credits, showing their own growth as musicians. The lyrics are more profound and the music more passionate. The “Youth Series” concludes with The most beautiful moment in life pt.2, but the boys are only now on the starting line. “Never Mind”, BTS is just getting started."
+}
+
+let infoDisplay8 = `
+  <h1> Album Name: ${myAlbum8["name"]} </h1>
+  <p> Cost: ${myAlbum8["price"]} </p>
+  <p> Fan Rating: ${myAlbum8["rating"]} out of 5 </p>
+`
+
+let summaryDisplay8 = `
+<p> Official Summary: ${myAlbum8["summary"]} </p>
+`
+$("#album-info8").append(infoDisplay4)
+$("#album-summary8").append(summaryDisplay4)
+let myAlbum9 = {
+  name: "THE MOST BEAUTIFUL MOMENT IN LIFE PT.1",
+  price: "$21.30",
+  rating: 4.6,
+  summary: "The most beautiful moment in life pt.1 focuses on the uncertain future more than the glamour of youth. In addition, while previous lyrical work had been credited to the rappers (RM, SUGA and j-hope), the vocal members (Jin, Jimin, V, Jung Kook) have now added their names to the credits for this album’s producing and songwriting."
+}
+
+let infoDisplay9 = `
+  <h1> Album Name: ${myAlbum9["name"]} </h1>
+  <p> Cost: ${myAlbum9["price"]} </p>
+  <p> Fan Rating: ${myAlbum9["rating"]} out of 5 </p>
+`
+
+let summaryDisplay9 = `
+<p> Official Summary: ${myAlbum9["summary"]} </p>
+`
+$("#album-info9").append(infoDisplay4)
+$("#album-summary9").append(summaryDisplay4)
+let myAlbum10 = {
+  name: "DARK & WILD",
+  price: "$23.98",
+  rating: 4.6,
+  summary: "The boys are no longer in their school uniforms. They have become tougher, singing that their love is not to be treated lightly. A year and two months into their debut, BTS made sure that the visuals, performance and all the elements of their first studio album are flawless. Tracks that highlight the different genres of hip hop—South, West, Boom Bap and Trap—provide a clear direction of BTS' music. The title track “Danger” is a tightly-composed hybrid hip hop song with a piercing punk-rock guitar sound that features stark rap, a powerful hook and exhilarating shouting."
+}
+
+let infoDisplay10 = `
+  <h1> Album Name: ${myAlbum10["name"]} </h1>
+  <p> Cost: ${myAlbum10["price"]} </p>
+  <p> Fan Rating: ${myAlbum10["rating"]} out of 5 </p>
+`
+
+let summaryDisplay10 = `
+<p> Official Summary: ${myAlbum10["summary"]} </p>
+`
+$("#album-info10").append(infoDisplay4)
+$("#album-summary10").append(summaryDisplay4)
+let myAlbum11 = {
+  name: "SKOOL LUV AFFAIR",
+  price: "$23.99",
+  rating: 5.0 ,
+  summary: "The songs spoke about young people living without a dream and schools that force rigid and homogenous thought onto its students. The new album, released on February 12, continues to focus on school and teens. As hinted by the title, SKOOL LUV AFFAIR, the album’s main theme is the love that blossoms in the school. This album is a culmination of the “School Trilogy” project that discusses the topics of most interest to teens their own age—dreams, happiness and love. Such penetrating insight into the hearts and minds of young people led to the absolute support that BTS has received from fans their age. The members again directly participated in the making of the songs, vowing to ensure that the songs reflect the reality of the music fans of their own generation."
+}
+
+let infoDisplay11 = `
+  <h1> Album Name: ${myAlbum11["name"]} </h1>
+  <p> Cost: ${myAlbum11["price"]} </p>
+  <p> Fan Rating: ${myAlbum11["rating"]} out of 5 </p>
+`
+
+let summaryDisplay11 = `
+<p> Official Summary: ${myAlbum11["summary"]} </p>
+`
+$("#album-info11").append(infoDisplay4)
+$("#album-summary11").append(summaryDisplay4)
+let myAlbum12 = {
+  name: "O!RUL8,2?",
+  price: "$19.53",
+  rating: 4.2,
+  summary: "With O!RUL8,2? (Oh! Are you late, too?), BTS offers the message that “you must find your happiness and your own life before it’s too late.” Lyrics that brutally lay open reality as it is deliver a bold admonition to a world that inflicts despair. Children are raised as studying machines without a chance to think about the future. A world where classmates are not friends but competitors trying to trample each other. BTS sings about the wounds and anguish that such hopeless life has left on today’s teens. ."
+}
+
+let infoDisplay12 = `
+  <h1> Album Name: ${myAlbum12["name"]} </h1>
+  <p> Cost: ${myAlbum12["price"]} </p>
+  <p> Fan Rating: ${myAlbum12["rating"]} out of 5 </p>
+`
+
+let summaryDisplay12 = `
+<p> Official Summary: ${myAlbum12["summary"]} </p>
+`
+$("#album-info12").append(infoDisplay4)
+$("#album-summary12").append(summaryDisplay4)
+let myAlbum13 = {
+  name: "2 COOL 4 SKOOL",
+  price: "$19",
+  rating: 4.8,
+  summary: "Hip hop group BTS has released its debut single 2 COOL 4 SKOOL. While billed as a single, the album boasts an impressive 27-minute runtime. This is the product of the firm conviction and passion of BTS that hip hop musicians must make their voices heard through their albums. Each member flexed their musical prowess by taking part in creating the lyrics and music for every song on the album. BTS has repeatedly said that “hip hop is genuine music for telling one’s own story,” and indeed the members rap about their own experiences and emotions. There is no discontinuity between the emotions and lyrics of BTS. The album is a satisfaction in both quality and quantity."
+}
+
+let infoDisplay13 = `
+  <h1> Album Name: ${myAlbum13["name"]} </h1>
+  <p> Cost: ${myAlbum13["price"]} </p>
+  <p> Fan Rating: ${myAlbum13["rating"]} out of 5 </p>
+`
+
+let summaryDisplay13 = `
+<p> Official Summary: ${myAlbum13["summary"]} </p>
+`
+$("#album-info13").append(infoDisplay4)
+$("#album-summary13").append(summaryDisplay4)
+
 });
